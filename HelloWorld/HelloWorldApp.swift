bdbe32583a0affd53d7381f0e28f5aa08fb06579
Vanessa Isabel Roque <vroque19@csu.fullerton.edu>
2024-11-09 19:40:38 -0800

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Vanessa Roque on 11/9/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
